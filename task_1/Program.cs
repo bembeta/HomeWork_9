aa09013190bc62719ad74e22ef98dd8d09481093
stinqyy <maxvik19m@mail.ru>
2023-02-13 17:46:11 +0300

    Выполнил первое задание

---
+++ task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+Console.WriteLine("Ввидите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int a = 0;
+int Recyrc(int n, int a)
+{
+    if(n == a) 
+        return n;
+    else
+        Console.Write($"{Recyrc(n, a + 1)} ");
+        return a;
+}
+Recyrc(n, a);
